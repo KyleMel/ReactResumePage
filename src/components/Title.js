36cbd68e12fc4e04e1c00e9f31e3aa1a91b043c6
Kyle Melton <87396239+KyleMel@users.noreply.github.com>
2021-11-23 11:59:07 -0600

    Creates working Links in banner

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,8 +5,8 @@ import styles from "./Title.module.css"
 const TopBanner = () => {
   return (
     <header>
-      <Link to ="https://github.com/KyleMel" className={styles.bannerLink}>Github</Link>
-      <Link to ="https://www.linkedin.com/in/kyle-melton-1a1541162/" className={styles.bannerLink}>LinkedIn</Link>
+      <a className={styles.bannerLink} href="https://github.com/KyleMel" target="Kyles Github">Github</a>
+      <a className={styles.bannerLink} href="https://www.linkedin.com/in/kyle-melton-1a1541162/" target="Kyles LinkedIn">LinkedIn</a>
     </header>
   );
 }
